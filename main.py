4a9637ab245d78ba3e7bc005d79f0324b62146d2
KLD997 <kld997@outlook.com>
2025-08-12 17:37:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import QApplication
 
 if __name__ == "__main__":
         app = QApplication(sys.argv)
-        window = LinOLS()
+        window = FreeTune()
         window.show()
         exit_code = app.exec()
         window.deleteLater()
